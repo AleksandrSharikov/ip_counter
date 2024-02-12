215c257bf45c9b5eef9b34c4dacab899de684971
AleksandrSharikov <Aleksandr.N.Sharikov@gmail.com>
2024-02-12 14:23:20 +0300

    Added two methods

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,35 @@ import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashSet;
-import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         String filename = "src/ip_test.txt";        // file name
-        Set<Integer> set = new HashSet<>();
+        var set = new HashSet<Integer>(){
+            public boolean contains(String ipStr){          // Overloaded method to check IP as a string
+                int check = 0;
+                int i = 0;
+                String[] octetStr = ipStr.split("\\.");
+                for (String octet : octetStr){
+                    check |= Integer.parseInt(octet) << (8 * (3 - i++) );
+                }
+                return super.contains(check);
+            }
+
+            @Override
+            public String toString() {                          // to see a list of distinct IPs
+                StringBuilder answer = new StringBuilder();
+                for (int record : this) {
+                    for (int i = 3; i >= 0; i--) {
+                        answer.append((record >> 8 * i) & 0xFF) ;
+                        answer.append(i == 0 ? '\n' : '.');
+                    }
+                }
+                answer.deleteCharAt(answer.length() - 1);
+                return answer.toString();
+            }
+
+        };
 
         try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(filename))) {
             int octet = 0;
@@ -33,6 +56,10 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.printf("File contains %d distinct IPs", set.size());
+        System.out.printf("File contains %d distinct IPs \n", set.size());
+     //   System.out.printf("Set contains: \n%s\n",set.toString());
+        System.out.printf("It contains 192.0.2.14 = %b\n",set.contains("192.0.2.14"));
+        System.out.printf("It contains 192.0.2.15 = %b",set.contains("192.0.2.17"));
+
     }
 }
