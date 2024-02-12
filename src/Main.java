4da8df20d6ac3e19ed4ff8cb6ae6751994743e2b
AleksandrSharikov <Aleksandr.N.Sharikov@gmail.com>
2024-02-12 14:55:41 +0300

    Minor enhancements

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
-        String filename = "src/ip_test.txt";        // file name
+        String filename = "src/ip_test.txt";        // File path
         var set = new HashSet<Integer>(){
             public boolean contains(String ipStr){          // Overloaded method to check IP as a string
                 int check = 0;
@@ -18,7 +18,7 @@ public class Main {
             }
 
             @Override
-            public String toString() {                          // to see a list of distinct IPs
+            public String toString() {                          // To see a list of distinct IPs
                 StringBuilder answer = new StringBuilder();
                 for (int record : this) {
                     for (int i = 3; i >= 0; i--) {
@@ -50,16 +50,18 @@ public class Main {
                     octet = octet * 10 + ch - 48;
                 }
             }
-            ipInt = (ipInt << 8) | octet;       // assume there is not '/n' after the last row
+            ipInt = (ipInt << 8) | octet;       // Assume there is not '\n' after the last row
             set.add(ipInt);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
         System.out.printf("File contains %d distinct IPs \n", set.size());
-     //   System.out.printf("Set contains: \n%s\n",set.toString());
-        System.out.printf("It contains 192.0.2.14 = %b\n",set.contains("192.0.2.14"));
-        System.out.printf("It contains 192.0.2.17 = %b",set.contains("192.0.2.17"));
+     //   System.out.printf("Set contains: \n%s\n",set.toString());    // Commented to not spoil the result output
+        String test1 = "192.0.2.14";
+        String test2 = "192.0.2.17";
+        System.out.printf("It contains %s = %b\n", test1, set.contains(test1));
+        System.out.printf("It contains %s = %b", test2, set.contains(test2));
 
     }
 }
